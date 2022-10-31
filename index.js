f4a3a81f06e4bc5127e98bb82ae338bf7bd7b04d
Alan Park <alanp@Alans-Laptop.local>
2022-10-30 17:34:16 -0700

    Recipe endpoint, TODO: right now is currently hardcoded to specific query

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://tasty.p.rapidapi.com/recipes/list',
+  params: {from: '0', size: '20', tags: 'under_30_minutes'},
+  headers: {
+    'X-RapidAPI-Key': '0fabe6247amsh3cc35d831f1063ep17f222jsncb85460e2453',
+    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+  }
+};
+
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -14,6 +26,17 @@ app.get('/', (req, res) => {
     res.json({ info: 'Test test'})
 });
 
+app.get('/recipes', (req, res) => {
+    axios.request(options).then(function (response) {
+        res.json(response.data)
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+});
+
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
 });
+
+
