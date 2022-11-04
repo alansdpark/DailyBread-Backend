ea5fb2018836bbc6ede4804f3ae4f6b9eab4aa8f
Alan Park <28246519+alansdpark@users.noreply.github.com>
2022-11-03 21:57:50 -0700

    Tasty API Integration (#2)
    * Recipe endpoint, TODO: right now is currently hardcoded to specific query
    
    * removed count from response
    
    * Only show certain data from response
    
    * Refactored parameters in recipe endpoint
    
    * Changed json response of ingredients/instructions
    
    * Fixed bug for certain queries

--- index.js
+++ index.js
@@ -17,6 +17,62 @@ app.get('/', (req, res) => {
     res.json({ info: 'Test test'})
 });
 
+app.get('/recipes', (req, res) => {
+    const query = req.query.q;
+
+    const options = {
+        method: 'GET',
+        url: 'https://tasty.p.rapidapi.com/recipes/list',
+        params: {
+            from: '0', 
+            size: '20', 
+            q: query
+        },
+        headers: {
+            'X-RapidAPI-Key': '0fabe6247amsh3cc35d831f1063ep17f222jsncb85460e2453',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    axios.request(options).then(function (response) {
+        const resultsParsed = response.data.results.map(resultsObject => {
+
+            let instructions = [];
+            let ingredients = [];
+
+            const hasInstructions = resultsObject.hasOwnProperty('instructions'); 
+            if (hasInstructions) {
+                resultsObject.instructions.map(instructionsObject => {
+                    instructions.push(instructionsObject.display_text);
+                });
+            }
+            
+            let hasIngredients = false;
+            if (resultsObject.hasOwnProperty('sections')) {
+                hasIngredients = resultsObject.sections[0].hasOwnProperty('components');
+                if (hasIngredients) {
+                    resultsObject.sections[0].components.map(ingredientsObject => {
+                        ingredients.push(ingredientsObject.raw_text);
+                    });
+                }
+            }
+            
+            if (hasInstructions || hasIngredients) { // only return objects that have at least instructions or ingredients
+                return {
+                    id: resultsObject.id,
+                    name: resultsObject.name,
+                    instructions: instructions,
+                    ingredients: ingredients
+                }
+            } 
+        });
+        const filteredResponse = resultsParsed.filter(obj => obj); // Filter out null values
+        res.json(filteredResponse);
+    }).catch(function (error) {
+        console.error(error);
+    });
+});
+
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
 });
