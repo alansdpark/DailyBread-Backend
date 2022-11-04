a94057dc3f20ac1ce111dc1b448d251860126a22
Alan Park <28246519+alansdpark@users.noreply.github.com>
2022-11-03 21:55:34 -0700

    Fixed bug for certain queries

--- index.js
+++ index.js
@@ -6,16 +6,6 @@ const Client = require("./postgres/Client");
 const app = express();
 const port = 3000;
 
-const options = {
-  method: 'GET',
-  url: 'https://tasty.p.rapidapi.com/recipes/list',
-  params: {from: '0', size: '20', q: 'apple, flour'},
-  headers: {
-    'X-RapidAPI-Key': '0fabe6247amsh3cc35d831f1063ep17f222jsncb85460e2453',
-    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-  }
-};
-
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -28,26 +18,56 @@ app.get('/', (req, res) => {
 });
 
 app.get('/recipes', (req, res) => {
+    const query = req.query.q;
+
+    const options = {
+        method: 'GET',
+        url: 'https://tasty.p.rapidapi.com/recipes/list',
+        params: {
+            from: '0', 
+            size: '20', 
+            q: query
+        },
+        headers: {
+            'X-RapidAPI-Key': '0fabe6247amsh3cc35d831f1063ep17f222jsncb85460e2453',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+
     axios.request(options).then(function (response) {
         const resultsParsed = response.data.results.map(resultsObject => {
+
             let instructions = [];
-            resultsObject.instructions.map(instructionsObject => {
-                instructions.push(instructionsObject.display_text);
-            });
-
             let ingredients = [];
-            resultsObject.sections[0].components.map(ingredientsObject => {
-                ingredients.push(ingredientsObject.raw_text);
-            });
 
-            return {
-                id: resultsObject.id,
-                name: resultsObject.name,
-                instructions: instructions,
-                ingredients: ingredients
+            const hasInstructions = resultsObject.hasOwnProperty('instructions'); 
+            if (hasInstructions) {
+                resultsObject.instructions.map(instructionsObject => {
+                    instructions.push(instructionsObject.display_text);
+                });
             }
+            
+            let hasIngredients = false;
+            if (resultsObject.hasOwnProperty('sections')) {
+                hasIngredients = resultsObject.sections[0].hasOwnProperty('components');
+                if (hasIngredients) {
+                    resultsObject.sections[0].components.map(ingredientsObject => {
+                        ingredients.push(ingredientsObject.raw_text);
+                    });
+                }
+            }
+            
+            if (hasInstructions || hasIngredients) { // only return objects that have at least instructions or ingredients
+                return {
+                    id: resultsObject.id,
+                    name: resultsObject.name,
+                    instructions: instructions,
+                    ingredients: ingredients
+                }
+            } 
         });
-        res.json(resultsParsed);
+        const filteredResponse = resultsParsed.filter(obj => obj); // Filter out null values
+        res.json(filteredResponse);
     }).catch(function (error) {
         console.error(error);
     });
